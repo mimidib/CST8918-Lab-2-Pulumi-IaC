5168aa39b7e88255d63c46120583d73860f22c9c
mimidib <dib00016@algonquinlive.com>
2026-05-22 16:52:09 -0400

    CST8918: add backend docker image RemoteImage (pull) to pulumi entrypoint

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+
+const stack = pulumi.getStack();
+
+// Pull the backend image
+const backendImageName = "backend";
+const backend = new docker.RemoteImage(`${backendImageName}Image`, {
+  name: "pulumi/tutorial-pulumi-fundamentals-backend:latest",
+});
