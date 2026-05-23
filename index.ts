7e73261428c8830fcd9297b2a5143fc28e1059d3
mimidib <dib00016@algonquinlive.com>
2026-05-23 12:17:27 -0400

    CST8918: Add frontend and MongoDB image pulls

--- index.ts
+++ index.ts
@@ -8,3 +8,14 @@ const backendImageName = "backend";
 const backend = new docker.RemoteImage(`${backendImageName}Image`, {
   name: "pulumi/tutorial-pulumi-fundamentals-backend:latest",
 });
+
+// Pull the frontend image
+const frontendImageName = "frontend";
+const frontend = new docker.RemoteImage(`${frontendImageName}Image`, {
+  name: "pulumi/tutorial-pulumi-fundamentals-frontend:latest",
+});
+
+// Pull the MongoDB image
+const mongoImage = new docker.RemoteImage("mongoImage", {
+  name: "pulumi/tutorial-pulumi-fundamentals-database:latest",
+});
